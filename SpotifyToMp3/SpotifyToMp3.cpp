0b4881e427f22874e855bac0a9d38038436233df
AlexRuzin <stan.ruzin@gmail.com>
2019-10-19 12:20:24 -0400

    Major integration with LAME encoder & PortAudio libaries, created initial encoding classes

--- SpotifyToMp3/SpotifyToMp3.cpp
+++ SpotifyToMp3/SpotifyToMp3.cpp
@@ -5,14 +5,11 @@
 #include <string>
 #include <signal.h> 
 
+#include "recordToMp3.h"
 #include "streamReader.h"
-
-// Default parameters
-const int lameEncoderQuality = 2;
 
 // Prototypes
 void signal_handler(int signal);
-void debug(const char* d...);
 void exitPaError(PaError err);
 
 int main()
@@ -21,8 +18,6 @@ int main()
 	typedef void (*SignalHandlerPointer)(int);
 	SignalHandlerPointer previousHandler;
 	previousHandler = signal(SIGABRT, signal_handler);
-
-	streamReader reader{ inputChannels, outputChannels, sampleRate, bufferLength };
 
 	static_assert(sizeof(int) == 4, "lame support requires 32 bit");
 	
@@ -34,132 +29,7 @@ int main()
 	return 0;
 }
 
-class recordToMp3 {
-public:
-	const char *filename;
-	
-friend:
-	char *deviceName;
-	int targetDevice;
-	double streamRate;
-	
-	PaDeviceInfo *device;
-	PaStream *pAStream;
-	PaStreamParameters *streamParams;
-	
-	mp3Encoder *encoder;
-	
-public:
-	recordToMp3(const char *filename) :
-		targetDevice(0),
-		pAStream(NULL)
-	{
-		if (filename == NULL) {
-			debug("[!] filename cannot be NULL");
-			ExitProcess(1);
-		}
-		filename = filename;
-	}
-	
-	~recordToMp3()
-	{
-		Pa_CloseStream(this->pAStream);
-	}
-	
-	int selectPrimaryDevice(const char *deviceName)
-	{
-		this->deviceName = const_cast<char *>(deviceName);
-		const int deviceCount = Pa_GetDeviceCount();
-		for (int i = 0; i < deviceCount; i++) {
-			if (std::string(Pa_GetDeviceInfo(i)->name).find(deviceName) != std::string::npos) {
-				this->device = Pa_GetDeviceInfo(i)
-				cout << "[+] Using device: " << this->device << std::endl;
-				this->targetDevice = i;
-			}
-		}
-		
-		this->streamRate = this->device->defaultSampleRate;
-		
-		return 0;
-	}
-	
-	int openIntoStream()
-	{
-		this->streamParams = new(PaStreamParameters);
-		streamParams->channelCount = this->device->maxInputChannels;
-		streamParams->device = targetDevice;
-		streamParams->hostApiSpecificsStreamInfo = NULL;
-		streamParams->sampleFormat = paInt32|paNonInterleaved;
-		streamParams->suggestedLatency = this->device->defaultLowInputLatency;
-		
-		this->encoder = new mp3Encoder(sampleRate, streamParams->channelCount, 128000, this->fileName);
-		
-		int err = Pa_OpenStream(this->pAStream, 
-					streamParams, 
-					NULL, 
-					this->streamRate, 
-					paNoFlag, 
-					this->encoder->paCallback_i32);
-		if (!err) {
-			debug("[!] Error opening PA stream");
-			ExitProcess(0);
-		} 
-		err = Pa_StartStream(this->pAStream);
-		if (!err) {
-			debug("[!] Error in starting PA stream");
-			ExitProcess(0);
-		}
-		
-		return 0;
-	}
-		
-private:
-	
-};
 
-class mp3Encoder : public recordToMp3 {
-public:
-	int sampleRate;
-	int channels;
-	int bitrate;
-	std::string fileName;
-	
-private:
-	std::vector<char> buf;
-	lame_global_flags *lameFlags;
-	
-public:	
-	mp3Encoder(int sampleRate, int channels, int bitrate, std::string fileName) :
-	sampleRate(sampleRate),
-	channels(channels),
-	bitrate(bitrate),
-	fileName(fileName)
-	{
-		buf.resize(1024 * 64);
-		lameFlags = lame_init();
-		if (!lame_init) {
-			ExitProcess(-1);	
-		}
-		
-		lame_set_in_samplerate(lameFlags, sampleRate);
-		lame_set_num_channels(lameFlags, channels);
-		lame_set_out_samplerate(lameFlags, 0);
-		
-		lame_set_brate(lameFlags, lameEncoderQuality);
-		
-		lame_init_params(this->lameFlags);
-	}
-	
-	static paCallback_i32(const void * input, void *output,
-			      unsigned long frameCount, const PaStreamCallbackTimeInfo *timeInfo,
-			      PaStreamCallbackFlags statusFlags, void *userData)
-	{
-		mp3Encoder *encPtr = (mp3Encoder *)userData;
-		const int32_t **pc = (const int32_t**)input;
-		enc->encode_sep(pc[0],pc[1],frameCount);
-		return 0;
-	}
-};
 
 void signal_handler(int signal)
 {
@@ -167,12 +37,8 @@ void signal_handler(int signal)
 	{
 	case SIGINT:
 	default:
-		debug("[+] Caught exit signal");
+		std::cout << "[+] Caught exit signal" << std::endl;
 		ExitProcess(0);
 	}
 }
 
-void debug(const char* d...)
-{
-	std::cout << d;
-}
---
+++ SpotifyToMp3/recordToMp3.h
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <signal.h> 
+#include <fstream>
+#include <vector>
+
+#include <lame.h>
+#include <portaudio.h>
+
+// Default parameters
+const int lameEncoderQuality = 2;
+
+// Prototyping
+static void debug(const char* d...);
+
+class mp3Encoder {
+public:
+	int sampleRate;
+	int channels;
+	int bitrate;
+	std::string fileName;
+
+	std::ofstream onf;
+
+private:
+	std::vector<char> buf;
+	lame_global_flags* lameFlags;
+
+public:
+	mp3Encoder(int sampleRate, int channels, int bitrate, std::string fileName)
+	{
+		sampleRate = sampleRate;
+		channels = channels;
+		bitrate = bitrate;
+		fileName = fileName;
+
+		buf.resize(1024 * 64);
+		lameFlags = lame_init();
+		if (!lame_init()) {
+			ExitProcess(-1);
+		}
+
+		lame_set_in_samplerate(lameFlags, sampleRate);
+		lame_set_num_channels(lameFlags, channels);
+		lame_set_out_samplerate(lameFlags, 0);
+
+		lame_set_brate(lameFlags, lameEncoderQuality);
+
+		lame_init_params(this->lameFlags);
+	}
+
+	static int paCallback_i32(const void* input, void* output,
+		unsigned long frameCount, const PaStreamCallbackTimeInfo* timeInfo,
+		PaStreamCallbackFlags statusFlags, void* userData)
+	{
+		mp3Encoder* encPtr = (mp3Encoder*)userData;
+		const int32_t** pc = (const int32_t**)input;
+		encPtr->encode_sep(pc[0], pc[1], frameCount);
+
+		return 0;
+	}
+
+	void encode_inter(const  short* s, int samples)
+	{
+		int n = lame_encode_buffer_interleaved(lameFlags, (short int*)s, samples, (unsigned char*)&buf[0], buf.size());
+		onf.write(&buf[0], n);
+	}
+
+	void encode_sep(const int32_t* L, const int32_t* R, int samples)
+	{
+		int n = lame_encode_buffer_int(lameFlags, (int32_t*)L, (int32_t*)R, samples, (unsigned char*)&buf[0], buf.size());
+		onf.write(&buf[0], n);
+	}
+
+	void encode_sep(const int16_t* L, const int16_t* R, int samples)
+	{
+		int n = lame_encode_buffer(lameFlags, (int16_t*)L, (int16_t*)R, samples, (unsigned char*)&buf[0], buf.size());
+		onf.write(&buf[0], n);
+	}
+
+	void flush()
+	{
+		int n = lame_encode_flush(lameFlags, (unsigned char*)&buf[0], buf.size());
+		onf.write(&buf[0], n);
+	}
+};
+
+class recordToMp3 {
+public:
+	std::string filename;
+
+	char* deviceName;
+	int targetDevice;
+	double streamRate;
+
+	PaDeviceInfo* device;
+	PaStream* pAStream;
+	PaStreamParameters* streamParams;
+
+	mp3Encoder* encoder;
+
+public:
+	recordToMp3(const char* filename) :
+		targetDevice(0),
+		pAStream(NULL)
+	{
+		if (filename == NULL) {
+			debug("[!] filename cannot be NULL");
+			ExitProcess(1);
+		}
+		filename = filename;
+	}
+
+	~recordToMp3()
+	{
+		Pa_CloseStream(this->pAStream);
+	}
+
+	int selectPrimaryDevice(const char* deviceName)
+	{
+		this->deviceName = const_cast<char*>(deviceName);
+		const int deviceCount = Pa_GetDeviceCount();
+		for (int i = 0; i < deviceCount; i++) {
+			if (std::string(Pa_GetDeviceInfo(i)->name).find(deviceName) != std::string::npos) {
+				this->device = const_cast<PaDeviceInfo*>(Pa_GetDeviceInfo(i));
+					std::cout << "[+] Using device: " << this->device << std::endl;
+				this->targetDevice = i;
+			}
+		}
+
+		this->streamRate = this->device->defaultSampleRate;
+
+		return 0;
+	}
+
+	int openIntoStream()
+	{
+		this->streamParams = new(PaStreamParameters);
+		streamParams->channelCount = this->device->maxInputChannels;
+		streamParams->device = targetDevice;
+		streamParams->hostApiSpecificStreamInfo = NULL;
+		streamParams->sampleFormat = paInt32 | paNonInterleaved;
+		streamParams->suggestedLatency = this->device->defaultLowInputLatency;
+
+		this->encoder = new mp3Encoder(this->streamRate, streamParams->channelCount, 128000, this->filename);
+
+		int err = Pa_OpenStream(&this->pAStream,
+			streamParams,
+			NULL,
+			this->streamRate,
+			paFramesPerBufferUnspecified,
+			paNoFlag,
+			this->encoder->paCallback_i32,
+			(void *)this->encoder);
+		if (!err) {
+			debug("[!] Error opening PA stream");
+			ExitProcess(0);
+		}
+		err = Pa_StartStream(this->pAStream);
+		if (!err) {
+			debug("[!] Error in starting PA stream");
+			ExitProcess(0);
+		}
+
+		return 0;
+	}
+
+private:
+
+};
+
+static void debug(const char* d...)
+{
+	std::cout << d;
+}
